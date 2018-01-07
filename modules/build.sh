8e6f265f87d5e0225d2f0c7860040b18155676e4
Michael Clausen <encodeering@gmail.com>
2018-01-07 18:28:52 +0100

    removes micro version from image
    * micro patches should be implicitly applied

--- modules/build.sh
+++ modules/build.sh
@@ -10,15 +10,13 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
-SEMANTIC="${VERSION%.*}"
-
 docker-pull "$REPOSITORY/buildpack-$ARCH:jessie" "buildpack-deps:jessie"
 
-patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < "patch/$SEMANTIC/Dockerfile.patch"
+patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < "patch/$VERSION/Dockerfile.patch"
 
 docker build -t "${DOCKER_IMAGE}"                \
-             --build-arg NODE_VERSION="$VERSION" \
+             --build-arg NODE_VERSION="$PIN" \
              --build-arg NODE_ARCH="$NODE_ARCH"  \
-             "$PROJECT/$SEMANTIC"
+             "$PROJECT/$VERSION"
 
 docker run --rm "${DOCKER_IMAGE}" node --version
