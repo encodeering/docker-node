3a5ff6489f91b63f7baf885d54517a2a568c77b6
Michael Clausen <encodeering@gmail.com>
2018-01-07 18:58:26 +0100

    updates nodejs versions
    * replaces node 5 with node 9, node 5 is not supported
    * patches and configuration are not required anymore
    
    NOTE
    
    * node 4 is currently used by semaphore image, but needs to be changed to handle 4.8 instead

--- modules/build.sh
+++ modules/build.sh
@@ -10,13 +10,8 @@ set -e
 import com.encodeering.docker.config
 import com.encodeering.docker.docker
 
-docker-pull "$REPOSITORY/buildpack-$ARCH:jessie" "buildpack-deps:jessie"
+docker-pull "$REPOSITORY/buildpack-$ARCH:stretch" "buildpack-deps:stretch"
 
-patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < "patch/$VERSION/Dockerfile.patch"
-
-docker build -t "${DOCKER_IMAGE}"                \
-             --build-arg NODE_VERSION="$PIN" \
-             --build-arg NODE_ARCH="$NODE_ARCH"  \
-             "$PROJECT/$VERSION"
+docker build -t "${DOCKER_IMAGE}" "$PROJECT/${VERSION%.*}/stretch"
 
 docker run --rm "${DOCKER_IMAGE}" node --version
